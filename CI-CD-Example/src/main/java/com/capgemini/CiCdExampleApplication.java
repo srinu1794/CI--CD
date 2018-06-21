b15b98aa63df8a585fbebcf57d979c68dcf40e34
Srinivasulu Reddy <sdwarak8877@gmail.com>
2018-06-21 14:29:09 +0530

    new project

---
+++ CI-CD-Example/src/main/java/com/capgemini/CiCdExampleApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdExampleApplication.class, args);
+	}
+}
